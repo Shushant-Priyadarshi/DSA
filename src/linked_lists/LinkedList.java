76d38806f7a7ce35c1c2662a4fe516757a5b5a15
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-24 01:57:24 +0530

    learned creation of single LL

--- src/linked_lists/LinkedList.java
+++
@@ -1,7 +0,0 @@
-package linked_lists;
-
-public class LinkedList {
-    public static void main(String[] args) {
-        System.out.println("Linked List");
-    }
-}
---
+++ src/linked_lists/singlyLL/creation/SinglyLinkedList.java
@@ -0,0 +1,10 @@
+package linked_lists.singlyLL.creation;
+
+public class SinglyLinkedList {
+    private Node head;
+    private int size;
+
+    public int size(){
+        return this.size;
+    }
+}
