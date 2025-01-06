9ba72510844567386b23a4606bc8de260c7316a6
Shushant <profshushant@gmail.com>
2025-01-06 12:52:25 +0530

    learning patterns-2

--- src/basics/patterns/Main.java
+++ src/basics/patterns/Main.java
@@ -2,7 +2,11 @@ package basics.patterns;
 
 public class Main {
     public static void main(String[] args) {
-       // Patterns.Square();
-        Patterns.Triangle(5);
+        // Patterns.Square();
+        //Patterns.Triangle(5);
+        //Patterns.TriangleNumber(4);
+        //Patterns.SameNumTriangle(4);
+        //Patterns.ReverseTriangle(4);
+        Patterns.ReverseTriangleNumber(5);
     }
 }
--- src/basics/patterns/Patterns.java
+++ src/basics/patterns/Patterns.java
@@ -4,7 +4,7 @@ public class Patterns {
     public static void Square() {
         for(int i = 0; i< 4; i++){
             for(int j=0;j<4; j++){
-                System.out.print(" * ");
+                System.out.print("* ");
             }
             System.out.println();
         }
@@ -14,6 +14,42 @@ public class Patterns {
         for(int i=0 ;i <n ;i++){
             for(int j=0 ; j <=i ; j++){
                 System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void TriangleNumber(int n){
+        for(int i=1; i<=n ;i++){
+            for(int j=1 ; j<=i ;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void SameNumTriangle(int n){
+        for(int i=1 ; i<=n ;i++){
+            for(int j =1 ; j<=i ; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ReverseTriangle(int n){
+        for(int i =0; i<n;i++){
+            for(int j=0; j< n-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void ReverseTriangleNumber(int n){
+        for(int i=0 ; i <n ; i++){
+            for(int j=1 ; j<=n-i;j++){
+                System.out.print(j+" ");
             }
             System.out.println();
         }
