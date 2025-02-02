72393031a9aef2451b478594333781be623e4928
Shushant <profshushant@gmail.com>
2025-02-03 00:12:32 +0530

    bub and sel

---
+++ src/sorting/Main.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {89,45,20,8,99,32};
+        System.out.println("Selection Sort: ");
+        Sorting.SelectionSort(arr);
+        System.out.println();
+        System.out.println();
+        System.out.println("Bubble Sort: ");
+        Sorting.BubbleSort(arr);
+
+    }
+}
